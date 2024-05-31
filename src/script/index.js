e2fb60cf47a53c5b50da5b31e3c31c983b2c9262
Arpit <arpit.mishra.out@gmail.com>
2024-05-31 18:46:52 +0530

    get acess to input elements

---
+++ src/script/index.js
@@ -0,0 +1,28 @@
+`use strict`;
+
+// ********************************
+// Form Valiation
+// getting acess to the dom elements
+
+const firstName = document.getElementById(`f-name`);
+
+const lastName = document.getElementById(`l-name`);
+
+const email = document.getElementById(`email`);
+
+// selecting radio buttons
+
+const queryOptions = document.querySelectorAll(`input[name = "query"]`);
+
+// selecting the text msg
+
+const message = document.getElementById(`msg`);
+
+// ************************************
+const checkBox = document.getElementById(`consent`);
+
+// *************************************
+
+const submiBtn = document.getElementById(`btn`);
+
+// *******************************************
