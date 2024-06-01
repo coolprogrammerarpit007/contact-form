ae592000c9c589c5f2671837dd344a9841ad80f9
Arpit <arpit.mishra.out@gmail.com>
2024-06-01 10:38:10 +0530

    commited validation and error functions to form

--- src/script/index.js
+++ src/script/index.js
@@ -26,3 +26,41 @@ const checkBox = document.getElementById(`consent`);
 const submiBtn = document.getElementById(`btn`);
 
 // *******************************************
+
+// selecting all the input elements
+
+const inputEl = document.querySelectorAll(`.input-element`);
+
+// function for applying on focus
+
+const validation = function (el, i) {
+  el.classList.remove(`hidden`);
+  document.getElementById(`err-msg-${i + 1}`).classList.add(`hidden`);
+  el.classList.add(`sucess`);
+};
+
+// function to check error
+
+const error = function (el, i) {
+  el.classList.remove(`sucess`);
+  el.classList.add(`error`);
+  document.getElementById(`err-msg-${i + 1}`).classList.remove(`hidden`);
+};
+
+// Applying event listener on hover
+
+inputEl.forEach((input, i) => {
+  // applying the event listener
+  input.addEventListener(`mouseover`, function (e) {
+    validation(input, i);
+  });
+});
+
+// Applying event listener on mouse leave
+
+inputEl.forEach((input, i) => {
+  // applying the event listener
+  input.addEventListener(`mouseleave`, function (e) {
+    error(input, i);
+  });
+});
