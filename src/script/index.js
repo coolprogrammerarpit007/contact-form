021c4b7b302a01209fb8a9810750a25abf406d0c
arpit mishra <arpit.mishra.out@gmail.com>
2024-06-03 07:21:13 +0530

    Added validation

--- src/script/index.js
+++ src/script/index.js
@@ -1,103 +1,120 @@
 `use strict`;
 
-// ********************************
-// Form Valiation
-// getting acess to the dom elements
 
-// state variable
-let isOption = false;
-let inputValue = false;
+let selectedValue;
+const inputEls = document.querySelectorAll(`.input-element`);
 
-const firstName = document.getElementById(`f-name`);
+// Now Adding focus event listener to the input elements
 
-const lastName = document.getElementById(`l-name`);
+inputEls.forEach((el)=>{
+  el.addEventListener(`focus`,(e)=>{
+    el.classList.add(`sucess`)
+  })
+})
 
-const email = document.getElementById(`email`);
 
-// selecting radio buttons
+// Event to on mouse leave
 
-const queryOptions = document.querySelectorAll(`input[name = "query"]`);
-
-// selecting the text msg
-
-const message = document.getElementById(`msg`);
-
-// ************************************
-const checkBox = document.getElementById(`consent`);
-
-// *************************************
-
-const submiBtn = document.getElementById(`btn`);
-
-// *******************************************
-
-// selecting all the input elements
-
-const inputEl = document.querySelectorAll(`.input-element`);
-
-// function for applying on focus
-
-const validation = function (el, i) {
-  el.classList.remove(`error`);
-  document.getElementById(`err-msg-${i + 1}`).classList.add(`hidden`);
-  el.classList.add(`sucess`);
-};
-
-// function to check error
-
-const error = function (el, i) {
-  el.classList.remove(`sucess`);
-  el.classList.add(`error`);
-  document.getElementById(`err-msg-${i + 1}`).classList.remove(`hidden`);
-};
-
-// *********************************
-// Applying event listener on hover
-
-inputEl.forEach((input, i) => {
-  // applying the event listener
-  input.addEventListener(`mouseover`, function (e) {
-    validation(input, i);
-  });
-});
-
-// Applying event listener on mouse leave
-
-inputEl.forEach((input, i) => {
-  // applying the event listener
-  input.addEventListener(`mouseleave`, function (e) {
-    const valueInput = input.value;
-    if (valueInput !== ``) {
-      validation(input, i);
-      inputValue = true;
-    } else {
-      error(input, i);
-      inputValue = false;
+inputEls.forEach((el) =>{
+  el.addEventListener(`mouseleave`,(e)=>{
+    if(el.classList.contains(`sucess`)){
+      el.classList.remove(`sucess`);
     }
-  });
-});
+  })
+})
 
-// **************************************
+
+// **************************************************
+// **************************************************
+
+// Adding Event Listener to the radio buttons
+
+const optionValues = document.querySelectorAll(`input[name = "query"]`);
+
 
 // Adding event listener to the radio btns
 
-queryOptions.forEach((query, i) => {
-  query.addEventListener(`change`, function (e) {
-    isOption = true;
-  });
-});
+optionValues.forEach((option)=>{
+  option.addEventListener(`change`,(e)=>{
+    if(option.checked){
+      selectedValue = option.value;
+      alert(selectedValue);
+    }
+  })
+})
 
-// *********************************
-// *********************************
 
-// Form submittion event
 
-submiBtn.addEventListener(`click`, function (e) {
-  e.preventDefault();
+// ************************************************
+// ************************************************
 
-  if (isOption && inputValue) {
-    confirm(`Your form has submitted sucessfully!`);
-  } else {
-    alert(`Fill the necessary details!`);
+// Getting Acess to the form element
+
+const form = document.getElementById(`form`);
+
+
+// Adding Submit Event to the form event
+
+
+form.addEventListener(`submit`,(e)=>{
+
+
+  e.preventDefault(); // Preventing Page to get refreshed.
+
+  const firstName = document.getElementById(`f-name`).value;
+  const lastName = document.getElementById(`l-name`).value;
+  const email = document.getElementById(`email`).value;
+  const message = document.getElementById(`msg`).value;
+  const checkBox = document.getElementById(`consent`);
+  
+  //  To check if input values are right
+
+  if(!firstName.toLowerCase().trim()){
+    alert(`Invalid Input!`)
+    document.getElementById(`f-name`).classList.add(`error`);
+    return;
   }
-});
+  if(!lastName.toLowerCase().trim()){
+    alert(`Invalid Input!`)
+    document.getElementById(`f-name`).classList.add(`error`);
+    return;
+  }
+
+  if(!message.toLowerCase().trim()){
+    alert(`Invalid Input!`);
+    return;
+  }
+
+  // checking if email is valid
+
+  if(!email.toLowerCase().trim() || !isValidEmail(email) ){
+    alert(`Invalid Email Address!`);
+    return;
+  }
+
+
+  // checking if the checkbox got checked or not!
+  if(!checkBox.checked){
+    alert(`You have to agree to consent!`);
+    return;
+  }
+
+
+  // checking if radio btns are selected or not!
+  if(!selectedValue){
+    alert(`Select a proper option!`);
+    return;
+  }
+
+  alert(`Your form has been submitted!`);
+  form.reset();
+})
+
+
+
+
+// function to check if email is valid or not
+
+function isValidEmail(email){
+  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+}
